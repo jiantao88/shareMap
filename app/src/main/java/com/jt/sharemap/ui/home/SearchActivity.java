4de3f6dff08baa0bd6c2c8327c4131b0b40fb0d2
zhangjiantao <zhangjiantao@chehejia.com>
2018-05-24 10:31:27 +0800

    searcheview

--- app/src/main/java/com/jt/sharemap/ui/home/SearchActivity.java
+++ app/src/main/java/com/jt/sharemap/ui/home/SearchActivity.java
@@ -1,7 +1,14 @@
 package com.jt.sharemap.ui.home;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
+import com.jt.sharemap.R;
 import com.jt.sharemap.ui.base.BaseActivity;
 
 /**
@@ -13,9 +20,50 @@ import com.jt.sharemap.ui.base.BaseActivity;
  * </pre>
  */
 public class SearchActivity extends BaseActivity{
+    private SearchView mSearchView;
+    private SearchView.SearchAutoComplete searchAutoComplete;
+
     @Override
     protected void receiveEvent(Object object) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu_setting, menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_search);
+        //获取搜索框
+        mSearchView = (SearchView) menuItem.getActionView();
+        //设置搜索hint
+        mSearchView.setQueryHint(getString(R.string.search_keyword));
+        mSearchView.onActionViewExpanded();
+        searchAutoComplete = (SearchView.SearchAutoComplete) mSearchView.findViewById(R.id.search_src_text);
+        searchAutoComplete.setHintTextColor(ContextCompat.getColor(this, R.color._60ffffff));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ImageView mCloseView = (ImageView) mSearchView.findViewById(R.id.search_close_btn);
+            mCloseView.setBackground(ContextCompat.getDrawable(this, R.drawable.ripple_close));
+        }
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+//                keyword = query;
+//                refreshData();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+//                if (TextUtils.isEmpty(newText)) {
+//                    keyword = newText;
+//                    if (mHotwordDatas.size() == 0)
+//                        loadTagDatas();
+//                }
+
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -30,7 +78,7 @@ public class SearchActivity extends BaseActivity{
 
     @Override
     protected boolean initToolbar() {
-        return false;
+        return true;
     }
 
     @Override
